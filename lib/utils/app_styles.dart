6506a359b2677435d1954046068f735ca00beb79
FakhrElddin <fa1641@fayoum.edu.eg>
2024-07-21 04:32:25 +0300

    add text styles

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const TextStyle textStyleRegular12 = TextStyle(
+    color: Color(0xffAAAAAA),
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Montserrat',
+  );
+
+  static const TextStyle textStyleRegular14 = TextStyle(
+    color: Color(0xffAAAAAA),
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Montserrat',
+  );
+
+  static const TextStyle textStyleRegular16 = TextStyle(
+    color: Color(0xff064061),
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Montserrat',
+  );
+
+  static const TextStyle textStyleMedium16 = TextStyle(
+    color: Color(0xff064061),
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Montserrat',
+  );
+
+  static const TextStyle textStyleSemiBold16 = TextStyle(
+    color: Color(0xff064061),
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Montserrat',
+  );
+
+  static const TextStyle textStyleBold16 = TextStyle(
+    color: Color(0xff4EB7F2),
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'Montserrat',
+  );
+
+  static const TextStyle textStyleSemiBold18 = TextStyle(
+    color: Color(0xffFFFFFF),
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Montserrat',
+  );
+
+  static const TextStyle textStyleSemiBold20 = TextStyle(
+    color: Color(0xff064061),
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Montserrat',
+  );
+
+  static const TextStyle textStyleMedium20 = TextStyle(
+    color: Color(0xffFFFFFF),
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Montserrat',
+  );
+
+  static const TextStyle textStyleSemiBold24 = TextStyle(
+    color: Color(0xff4EB7F2),
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Montserrat',
+  );
+}
