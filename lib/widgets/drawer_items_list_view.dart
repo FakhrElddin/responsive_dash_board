6949f4c9aa2db70bceedf26bc50c017ddc24319e
FakhrElddin <fa1641@fayoum.edu.eg>
2024-07-22 11:27:35 +0300

    create active drawer item and toggle item state

--- lib/widgets/drawer_items_list_view.dart
+++ lib/widgets/drawer_items_list_view.dart
@@ -3,43 +3,59 @@ import 'package:responsive_dash_board/models/drawer_item_model.dart';
 import 'package:responsive_dash_board/utils/app_images.dart';
 import 'package:responsive_dash_board/widgets/drawer_item.dart';
 
-class DrawerItemsListView extends StatelessWidget {
+class DrawerItemsListView extends StatefulWidget {
   const DrawerItemsListView({
     super.key,
   });
 
-  static const List<DrawerItemModel> items = [
-    DrawerItemModel(
+  @override
+  State<DrawerItemsListView> createState() => _DrawerItemsListViewState();
+}
+
+class _DrawerItemsListViewState extends State<DrawerItemsListView> {
+  final List<DrawerItemModel> items = [
+    const DrawerItemModel(
       image: Assets.imagesDashboard,
       title: 'Dashboard',
     ),
-    DrawerItemModel(
+    const DrawerItemModel(
       image: Assets.imagesMyTransaction,
       title: 'My Transaction',
     ),
-    DrawerItemModel(
+    const DrawerItemModel(
       image: Assets.imagesStatistics,
       title: 'Statistics',
     ),
-    DrawerItemModel(
+    const DrawerItemModel(
       image: Assets.imagesWalletAccount,
       title: 'Wallet Account',
     ),
-    DrawerItemModel(
+    const DrawerItemModel(
       image: Assets.imagesMyInvestments,
       title: 'My Investments',
     ),
   ];
+  int activeIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
-      itemBuilder: (context, index) => Padding(
-        padding: const EdgeInsets.only(top: 20.0),
-        child: DrawerItem(
-          drawerItemModel: items[index],
+      itemBuilder: (context, index) => GestureDetector(
+        onTap: () {
+          if (activeIndex != index) {
+            setState(() {
+              activeIndex = index;
+            });
+          }
+        },
+        child: Padding(
+          padding: const EdgeInsets.only(top: 20.0),
+          child: DrawerItem(
+            drawerItemModel: items[index],
+            isActive: activeIndex == index ? true : false,
+          ),
         ),
       ),
       itemCount: items.length,
