ab1197949ba58591eccaa1a5d78dc1e229199968
FakhrElddin <fa1641@fayoum.edu.eg>
2024-07-27 18:23:43 +0300

    create my card and transaction history section

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_and_quick_invoice_section.dart';
 import 'package:responsive_dash_board/widgets/custom_darwer.dart';
-import 'package:responsive_dash_board/widgets/transaction_history.dart';
+import 'package:responsive_dash_board/widgets/my_card_and_transaction_history_section.dart';
 
 class DashboardDesktopLayout extends StatelessWidget {
   const DashboardDesktopLayout({super.key});
@@ -24,7 +24,14 @@ class DashboardDesktopLayout extends StatelessWidget {
           width: 24,
         ),
         Expanded(
-          child: TransactionHistory(),
+          child: Column(
+            children: [
+              MyCardAndTransactionHistorySection(),
+              SizedBox(
+                height: 24,
+              ),
+            ],
+          ),
         ),
       ],
     );
